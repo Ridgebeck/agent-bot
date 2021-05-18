334159e78f4f90640a0d5903c6606b77e0fc5b35
Niklas <beck.niklas@gmail.com>
2021-05-18 03:06:13 -0700

    add custom action for street, synonyms

--- actions/actions.py
+++ actions/actions.py
@@ -12,7 +12,8 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
 correct_answer_city = "Chicago"
-correct_answer_street = "Oak Street"
+correct_answer_last_street_guess = "First Street"
+correct_answer_street_2 = "Oak Street"
 correct_answer_3 = "123456"
 
 class ActionVerifycity(Action):
@@ -29,15 +30,12 @@ class ActionVerifycity(Action):
 
         if city == None:
             dispatcher.utter_message(text="I don't think that is a city. Did you spell correctly?")
-            #print("spelling issue?")
             return []
         elif city.lower() == correct_answer_city.lower():
             dispatcher.utter_message(text="{} is correct! \n We will send someone there. Thanks for your help!".format(correct_answer_city))
-            #print("correct city")
             return [SlotSet("solution_city", correct_answer_city)]
         else:
             dispatcher.utter_message(text="{} is wrong!".format(city))
-            #print("wrong city")
             return [SlotSet("city", None)]
 
 
@@ -50,11 +48,65 @@ class ActionVerifyStreet(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        #street = tracker.get_slot("street")
+        # get slot of last single street guess
+        last_street_guess = tracker.get_slot("last_street_guess")
+        print("last_street_guess: {} ".format(last_street_guess))
 
-        dispatcher.utter_message("This is a street name!")
-        return []
+        # list for detected street values
+        streets = []
 
+        # go through all entities    
+        for entity in tracker.latest_message['entities']:
+            # check if entity was detected as a street and append text value
+            if entity['entity'] == 'street':
+                streets.append(entity['value'])
+
+        print(streets)
+        # validate if solution is correct if two streets were given (both have to be correct)
+        if len(streets) == 2:            
+            if streets[0].lower() == correct_answer_last_street_guess.lower() and streets[1].lower() == correct_answer_street_2.lower() or streets[1].lower() == correct_answer_last_street_guess.lower() and streets[0].lower() == correct_answer_street_2.lower():
+                dispatcher.utter_message(text="Two street names were given: {} and {} - they are correct!".format(streets[0], streets[1]))
+                return [SlotSet("solution_street", "{} & {}".format(correct_answer_last_street_guess, correct_answer_street_2))]
+            else:
+                dispatcher.utter_message(text="Two street names were given: {} and {} - they are NOT correct!".format(streets[0], streets[1]))
+                return [SlotSet("last_street_guess", None)]
+
+        # validate if solution is correct if only one street was given
+        elif len(streets) == 1:
+            # if last_street_guess has no saved value
+            if last_street_guess == None:
+                dispatcher.utter_message(text="One street name was given: {}. What's the other street?".format(streets[0]))
+                return [SlotSet("last_street_guess", streets[0])]
+            # if there was a saved street name
+            else:
+                streets.append(last_street_guess)
+                if streets[0].lower() == correct_answer_last_street_guess.lower() and streets[1].lower() == correct_answer_street_2.lower() or streets[1].lower() == correct_answer_last_street_guess.lower() and streets[0].lower() == correct_answer_street_2.lower():
+                    dispatcher.utter_message(text="Two street names were given: {} and {} - they are correct!".format(streets[0], streets[1]))
+                    return [SlotSet("solution_street", "{} & {}".format(correct_answer_last_street_guess, correct_answer_street_2))]
+                else:
+                    dispatcher.utter_message(text="Two street names were given: {} and {} - they are NOT correct!".format(streets[0], streets[1]))
+                    return [SlotSet("last_street_guess", None)]
+                
+        # complain if 0 or more than 2 street names were given 
+        else:
+            dispatcher.utter_message(text="I don't get it. Can you please specify one specific intersection of two streets?")
+            return [SlotSet("last_street_guess", None)]
+
+
+
+        # check if solution is correct once two streets have been entered
+
+
+        # first street correct
+        #return [SlotSet("last_street_guess", correct_answer_last_street_guess), SlotSet("street_2", None)]
+        
+        # second street correct
+        #return [SlotSet("last_street_guess", None), SlotSet("street_2", correct_answer_street_2)]
+
+        # first and second street correct
+        #return [SlotSet("last_street_guess", correct_answer_last_street_guess), SlotSet("street_2", correct_answer_street_2)]
+
+        
         # if street.lower() == correct_answer_street.lower():
         #     dispatcher.utter_message(text="{} is correct!".format(correct_answer_street))
         #     return [SlotSet("solution_street", correct_answer_street)]
